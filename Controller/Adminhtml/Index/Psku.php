1d373e585a43f49dac44883feb88de5e0eb6ed12
kuldip-ladola <kuldip1241@gmail.com>
2023-11-21 16:43:15 +0530

    change regre exp according to mudpie account

--- Controller/Adminhtml/Index/Psku.php
+++ Controller/Adminhtml/Index/Psku.php
@@ -72,7 +72,7 @@ class Psku extends \Magento\Backend\App\Action
             if (count($productSku) > 0) {
                 foreach ($productSku as $sku) {
                     if ($sku != "") {
-                        $bd_sku = trim(preg_replace('/[^A-Za-z0-9]/', '_', $sku));
+                        $bd_sku = trim(preg_replace('/[^A-Za-z0-9-]/', '_', $sku));
                         $get_data = $this->datahelper->getImageSyncWithProperties($bd_sku, $property_id, $collection_value);
                         $getIsJson = $this->getIsJSON($get_data);
                         if (!empty($get_data) && $getIsJson) {
